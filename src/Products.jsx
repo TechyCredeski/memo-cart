0ff571c8b589df3401421b9526fc4ece0a4375f3
Credenski <theonlycredeski@gmail.com>
2023-05-06 18:45:46 +0100

    Requested products from API with Axios

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,7 +1,24 @@
-
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 export default function Products() {
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+        try {
+            axios.get("https://fakestoreapi.com/products")
+            .then(function(response) {
+                console.log(response)
+            })
+        } catch(error) {
+            console.log(error)
+        }
+    })
+
     return (
-        <div>Products</div>
+        <div>
+        <div>Products:</div>
+        <div>{product}</div>
+        </div>
     )
 }
