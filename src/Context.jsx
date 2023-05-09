778272d0281ee5d880f9a57b799e7c805a7987d6
Credenski <theonlycredeski@gmail.com>
2023-05-09 23:18:59 +0100

    useReducer Added

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,9 +1,12 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const Cartcontext = createContext()
 export const Context = (props) => {
+    const reducerc = [state, action] => {
+        switch (action.type) {
+    }
 
-    
+    const [state, dispatch] = useReducer(reducer, [])
     const info ={}
     return (
         <Cartcontext.Provider value={state}>{props.children}</Cartcontext.Provider>
