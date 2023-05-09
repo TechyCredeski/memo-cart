4be65f86e17b9793f2b9e5c0decb951f51218d12
Credenski <theonlycredeski@gmail.com>
2023-05-09 23:15:56 +0100

    info added

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,7 +3,8 @@ import { createContext } from "react";
 export const Cartcontext = createContext()
 export const Context = (props) => {
 
-    const state = []
+    
+    const info ={}
     return (
         <Cartcontext.Provider value={state}>{props.children}</Cartcontext.Provider>
     )
