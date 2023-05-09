abdbb6e791326b0668ab8d5be5193812dfc6ce60
Credenski <theonlycredeski@gmail.com>
2023-05-09 16:00:29 +0100

    rendered cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React from 'react'
 import Products from './Products'
+import Cart from './Cart'
 
 
 function App() {
 
   return (
+    <>
+    <Cart/>
     <Products/>
+    </>
   )
 }
 
