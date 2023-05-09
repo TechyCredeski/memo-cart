d37e887c6f397eb46baf76a7ae0e9c48ce745249
Credenski <theonlycredeski@gmail.com>
2023-05-09 23:35:09 +0100

    error fix

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,18 +1,23 @@
 import { createContext, useReducer } from "react";
 
-export const Cartcontext = createContext()
-export const Context = (props) => {
-    const reducer = (state, action) => {
-        switch (action.type) {
-            case "ADD_TO_CART":
-                
-            default: return state
-    }
+export const Cartcontext = createContext();
 
-    const [state, dispatch] = useReducer(reducer, [])
-    const info ={state, dispatch}
-    return (
-        <Cartcontext.Provider value={info}>{props.children}</Cartcontext.Provider>
-    )
+export const Context = (props) => {
+const reducer = (state, action) => {
+switch (action.type) {
+    case "ADD_TO_CART":
+    // TODO: handle adding item to cart
+    return state;
+    default:
+    return state;
 }
-}
+}; 
+
+const [state, dispatch] = useReducer(reducer, []);
+const info = { state, dispatch };
+return (
+<Cartcontext.Provider value={info}>
+    {props.children}
+</Cartcontext.Provider>
+);
+};
